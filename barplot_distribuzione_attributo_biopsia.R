7afbda84a4d12b0e8a36228b487961c039930076
AndreaPiancone <54837603+AndreaPiancone@users.noreply.github.com>
2020-01-19 20:23:43 +0100

    Script R barplot biopsy

---
+++ barplot_distribuzione_attributo_biopsia.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+library(dplyr)
+
+data <- read.csv("https://raw.githubusercontent.com/AndreaPiancone/Progetto_machine_learning/master/kag_risk_factors_cervical_cancer.csv")
+
+data$Biopsy <- ordered(data$Biopsy, levels = c(0,1))
+
+df <- df <- data %>%
+  group_by(Biopsy) %>%
+  summarise(counts = n())
+
+ggplot(df, aes(x = Biopsy, y = counts)) +
+  geom_bar(fill = "#0073C2FF", stat = "identity") +
+  geom_text(aes(label = counts), vjust = -0.3) + 
+  theme_bw()
